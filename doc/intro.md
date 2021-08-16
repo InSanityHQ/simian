71f263c525c96ff027496850879169f6b4a134f8
Houjun Liu <hliu@shabang.cf>
2021-08-16 16:17:52 -0700

    Monkey go hee hee

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simian
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simian/core_test.clj
@@ -0,0 +1,7 @@
+(ns simian.core-test
+  (:require [clojure.test :refer :all]
+            [simian.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
